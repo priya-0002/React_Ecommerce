e01f83b4a7e8339ea3534b9d7489eeaf93959479
priya-002 <priya.md0002@gmail.com>
2021-07-08 23:09:18 +0530

    first commit

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -11,14 +11,7 @@ const products=[
 const Products = () => {
     return (
         <main>
-            <Grid container justify="center" spacing={4}>
-                {products.map((product)=>(
-                    <Grid item key={product.id}  xs={12} sm={6} md={4} lg={3}>
-                  {product? <Product product={product}/>: null}
-                    
-                    </Grid>
-                 ) )}
-            </Grid>
+            
         </main>
     )
 }
