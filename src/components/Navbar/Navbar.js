4276f899310dd4b2923edb66fc28eef41f6ab014
priya-002 <priya.md0002@gmail.com>
2021-07-10 23:58:01 +0530

    changes

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,7 +15,24 @@ const Navbar = () => {
   const classes=useStyles();
     return (
         <div>
-     
+            <AppBar position="fixed" className={classes.appBar} color="inherit">
+      
+  <Toolbar>
+    
+    <Typography color="inherit" variant="h6" className={classes.title}>
+      <img src={logo}  alt="Commerce.js" height="25px" className={classes.image} />
+      Commerce.js
+    </Typography>
+    <div className="classes.grow"></div>
+    <div className="classes.button">
+    <IconButton aria-label="Show cart items" color="inherit">
+    <Badge badgeContent={2} color='secondary'>
+        <ShoppingCart/>
+    </Badge>
+    </IconButton>
+    </div>
+  </Toolbar>
+</AppBar>
         </div>
     )
 }
